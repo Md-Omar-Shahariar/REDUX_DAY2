900ad7b6b8b2e2a9a4637ad139b7f36e96755bd7
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-11-17 23:19:54 +0600

    BMI

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,16 @@
-console.log(Math.PI);
+const markWeight = 78;
+const johnWeight = 92;
+const markHeight = 1.69;
+const johnHeight = 1.95;
+
+const BMI = (Height, Weight) => {
+  const bmiCount = Weight / Height ** 2;
+  return bmiCount;
+};
+
+const markBmi = BMI(markHeight, markWeight);
+const johnBmi = BMI(johnHeight, johnWeight);
+
+const isMikeHigher = markBmi > johnBmi;
+
+console.log(markBmi, johnBmi, isMikeHigher);
